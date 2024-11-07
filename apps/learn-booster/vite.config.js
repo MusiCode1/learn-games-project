37ca880e88ca2844c5108795538961d8d3a6cbd8
Musicode1 <MusiCode3@gmail.com>
2024-11-07 16:35:44 +0200

    (learn-booster): טרנספילציה

--- apps/learn-booster/vite.config.js
+++ apps/learn-booster/vite.config.js
@@ -1,13 +1,21 @@
+
+
 /** @type {import('vite').UserConfig} */
 export default {
 
-    build:{
-        outDir: 'docs'
-    },
-    server: {
-        cors: {
-            origin: false,
-            methods: ['GET', 'POST', 'OPTIONS']
+    build: {
+        outDir: 'docs',
+        rollupOptions: {
+            input: {
+                main: 'src/main.js',
+                'app-booster': 'src/app-booster.js'
+            }
+        },
+        server: {
+            cors: {
+                origin: false,
+                methods: ['GET', 'POST', 'OPTIONS']
+            }
         }
     }
-  }
+}
