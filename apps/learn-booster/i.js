fd258be83c2eb2dcec1e6c6ba448cc0d027d1e39
Musicode1 <MusiCode3@gmail.com>
2024-12-24 12:13:43 +0200

    (learn-booster): תיקון קובץ הזרקה.

---
+++ apps/learn-booster/i.js
@@ -0,0 +1,14 @@
+// dev inject
+
+(() => {
+    function loadExternalScript(url) {
+        const script = document.createElement('script');
+        script.src = url;
+        script.async = true;
+        script.type = 'module';
+        document.head.appendChild(script);
+    }
+
+    const url = '//dev-server.dev:5173';
+    loadExternalScript(url);
+})();
